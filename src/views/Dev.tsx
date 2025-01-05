c815806fad15614aa9b95534811dda3177ad874a
amitlzkpa <amit.lzkpa@gmail.com>
2025-01-05 13:58:26 +0530

    New button in UI to delete all messages

--- src/views/Dev.tsx
+++ src/views/Dev.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import { FaRedo } from 'react-icons/fa';
-import { Button, Card, Flex, Input, Loader, Text } from '@mantine/core';
+import { FaRedo, FaTrash } from 'react-icons/fa';
+import { Button, Card, Divider, Flex, Input, Loader, Text } from '@mantine/core';
 
 import { Authenticated, Unauthenticated, AuthLoading, useAction, useMutation, useQuery } from "convex/react";
 import { useUser } from "@clerk/clerk-react";
@@ -32,6 +32,14 @@ export default function Dev() {
     setMsg(reverseText);
   };
 
+  const performAction_testAction_deleteMsgs = useAction(api.vsActions.testAction_deleteMsgs);
+
+  const onClickTest_deleteMsgs = async () => {
+    console.log("Deleting all messages...");
+    const deletedMsgIds = await performAction_testAction_deleteMsgs();
+    console.log(`Deleted ${deletedMsgIds.length} messages.`);
+  };
+
   return (
     <Flex w="100%" direction="column" align="center" gap="sm">
       <AuthLoading>
@@ -45,9 +53,6 @@ export default function Dev() {
       <Authenticated>
         <Flex w="60%" direction="column" align="center" gap="md" p="lg">
           <Flex w="100%" align="center" gap="sm">
-            <Text>
-              {user?.firstName ?? "You"}:
-            </Text>
             <Input
               value={msg}
               onChange={(event) => setMsg(event.currentTarget.value)}
@@ -71,17 +76,33 @@ export default function Dev() {
             </Button>
           </Flex>
 
+          <Divider my="md" />
+
           <Flex w="100%" direction="column" align="stretch" gap="xs">
-            {
-              (currUserVsMsgs ?? [])
-                .map((msg, idx) => {
-                  return (
-                    <Card w="100%" withBorder radius="xl">
-                      <Text key={idx}>{msg.msgContent}</Text>
-                    </Card>
-                  );
-                })
-            }
+            <Flex w="100%" justify="space-between" align="center" gap="xs">
+              <Text>
+                {user?.firstName ? `${user?.firstName}'s` : "Your"} messages
+              </Text>
+              <Button
+                onClick={onClickTest_deleteMsgs}
+                size="md"
+                variant="outline"
+              >
+                <FaTrash color="versee-purple" />
+              </Button>
+            </Flex>
+            <Flex w="100%" direction="column" align="stretch" gap="xs">
+              {
+                (currUserVsMsgs ?? [])
+                  .map((msg, idx) => {
+                    return (
+                      <Card w="100%" withBorder radius="xl">
+                        <Text key={idx}>{msg.msgContent}</Text>
+                      </Card>
+                    );
+                  })
+              }
+            </Flex>
           </Flex>
         </Flex>
       </Authenticated>
