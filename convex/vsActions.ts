f8cde8a66930187a978a1f6ecf0bd0cbad66a110
amitlzkpa <amit.lzkpa@gmail.com>
2025-01-05 21:15:38 +0530

    New action to exchange code for access token

--- convex/vsActions.ts
+++ convex/vsActions.ts
@@ -21,12 +21,33 @@ export const testAction_debugOne = action({
     const oauthLoginUrl = apiClient.getJWTUri(integratorKey, RedirectURI, oAuthBasePath);
 
     return oauthLoginUrl;
+  }
+});
 
-    // const scopes = [
-    //   oAuth.Scope.IMPERSONATION,
-    //   oAuth.Scope.SIGNATURE
-    // ];
-    // const redirectUrl = await apiClient.requestJWTApplicationToken(integratorKey, scopes, privateKeyFile, expiresIn);
+export const testAction_debugTwo = action({
+  args: {
+    authCode: v.string(),
+  },
+  handler: async (ctx, { authCode }) => {
+    const restApi = docusign.ApiClient.RestApi;
+    const oAuth = docusign.ApiClient.OAuth;
+    const basePath = restApi.BasePath.DEMO;
+    const oAuthBasePath = oAuth.BasePath.DEMO;
+    const apiClient = new docusign.ApiClient({
+      basePath: basePath,
+      oAuthBasePath: oAuthBasePath
+    });
+
+    const IntegratorKeyAuthCode = "e612e007-3539-464a-a972-797c81ce2088";
+    const ClientSecret = "603aed07-5ede-409c-8246-4539a219e1c2";
+    const code = authCode;
+
+    console.log("======================================");
+    console.log("code");
+    console.log(code);
+
+    const token = await apiClient.generateAccessToken(IntegratorKeyAuthCode, ClientSecret, code);
+    return token;
   }
 });
 
