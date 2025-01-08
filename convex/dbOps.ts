6ac279222299f9bdd98e0bfd3c94f17c063f7cdc
amitlzkpa <amit.lzkpa@gmail.com>
2025-01-09 01:09:19 +0530

    Make the upsertDocusignDataForUser function patch the existing document

--- convex/dbOps.ts
+++ convex/dbOps.ts
@@ -7,7 +7,7 @@ import { v } from "convex/values";
 export const getDocusignData = internalQuery({
   handler: async (ctx) => {
     const currUser = await ctx.auth.getUserIdentity();
-    if (!currUser) return [];
+    if (!currUser) return null;
     const docusignData = await ctx.db
       .query("docusignDataForUser")
       .filter((q) => q.eq(q.field("user.subject"), currUser.subject))
@@ -23,8 +23,20 @@ export const upsertDocusignDataForUser = internalMutation({
   handler: async (ctx, { docusignDataStr }) => {
     const docusignData = JSON.parse(docusignDataStr);
     const currUser = await ctx.auth.getUserIdentity();
+    if (!currUser) return null;
     const upsertData = { ...docusignData, user: currUser };
-    const newRecord = await ctx.db.insert("docusignDataForUser", upsertData);
-    return newRecord;
+    const exData = await ctx.db
+      .query("docusignDataForUser")
+      .filter((q) => q.eq(q.field("user.subject"), currUser.subject))
+      .first();
+    let retVal;
+    if (exData) {
+      await ctx.db.patch(exData._id, upsertData);
+      retVal = exData._id;
+    } else {
+      const newRecord = await ctx.db.insert("docusignDataForUser", upsertData);
+      retVal = newRecord;
+    }
+    return retVal;
   },
 });
