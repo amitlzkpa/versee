68e2fc0ee17cdae5662df4d4c91f6435ede3eb83
amitlzkpa <amit.lzkpa@gmail.com>
2025-01-08 13:33:13 +0530

    Working envelop send

--- convex/dbOps.ts
+++ convex/dbOps.ts
@@ -1,9 +1,22 @@
 import {
   query,
   mutation,
+  internalQuery,
   internalMutation
 } from "./_generated/server";
 import { v } from "convex/values";
+
+export const getDocusignData = internalQuery({
+  handler: async (ctx) => {
+    const currUser = await ctx.auth.getUserIdentity();
+    if (!currUser) return [];
+    const docusignData = await ctx.db
+      .query("docusignDataForUser")
+      .filter((q) => q.eq(q.field("user.subject"), currUser.subject))
+      .first();
+    return docusignData;
+  },
+});
 
 export const upsertDocusignDataForUser = internalMutation({
   args: {
--- convex/vsActions.ts
+++ convex/vsActions.ts
@@ -1,6 +1,8 @@
 "use node";
+import * as https from "https";
 import { action } from "./_generated/server";
-import { internal } from "./_generated/api";
+import { api, internal } from "./_generated/api";
+import { Id } from "./_generated/dataModel";
 import { v } from "convex/values";
 
 import * as docusign from "docusign-esign";
@@ -67,8 +69,95 @@ export const testAction_deleteMsgs = action({
   },
 });
 
+async function downloadFileAsBytes(url: string): Promise<Buffer> {
+  return new Promise<Buffer>((resolve, reject) => {
+    https.get(url, (response) => {
+      const chunks: Buffer[] = [];
+      response.on('data', (chunk) => {
+        chunks.push(chunk);
+      });
+      response.on('end', () => {
+        const buffer = Buffer.concat(chunks);
+        resolve(buffer);
+      });
+    }).on('error', (err) => {
+      reject(err);
+    });
+  });
+}
+
 export const testAction_sendSigningEmail = action({
   handler: async (ctx) => {
-    return "foo";
+    const restApi = docusign.ApiClient.RestApi;
+    const oAuth = docusign.ApiClient.OAuth;
+    const basePath = restApi.BasePath.DEMO;
+    const oAuthBasePath = oAuth.BasePath.DEMO;
+    const dsApiClient = new docusign.ApiClient({
+      basePath: basePath,
+      oAuthBasePath: oAuthBasePath
+    });
+
+    const storedDocusignData = await ctx.runQuery(internal.dbOps.getDocusignData);
+    const accountId = storedDocusignData.userInfo.accounts[0].accountId;
+    const accessToken = storedDocusignData.oAuthToken.accessToken;
+
+    const storageId = "kg20kf8cv8m6stc4nct8cbdbah781b38" as Id<"_storage">;
+    const docUrl = (await ctx.runMutation(api.uploadedFiles.generateViewUrl, { storageId })) ?? "";
+    const docBytes = await downloadFileAsBytes(docUrl);
+    const doc3b64 = Buffer.from(docBytes).toString('base64');
+
+    dsApiClient.addDefaultHeader('Authorization', 'Bearer ' + accessToken);
+    const envelopesApi = new docusign.EnvelopesApi(dsApiClient);
+
+
+    // const envDef = await envelopesApi.createEnvelope(accountId);
+    const envDef = new docusign.EnvelopeDefinition();
+    envDef.emailSubject = 'Please Sign my Node SDK Envelope';
+    envDef.emailBlurb = 'Hello, Please sign my Node SDK Envelope.';
+
+    // add a document to the envelope
+    const doc = new docusign.Document();
+    const base64Doc = doc3b64;
+    doc.documentBase64 = base64Doc;
+    doc.name = 'TestFile.pdf';
+    doc.documentId = '1';
+
+    const docs = [];
+    docs.push(doc);
+    envDef.documents = docs;
+
+    // Add a recipient to sign the document
+    const signer = new docusign.Signer();
+    signer.email = "amit.lzkpa@gmail.com";
+    signer.name = 'Amit N';
+    signer.recipientId = '1';
+
+    // create a signHere tab somewhere on the document for the signer to sign
+    // default unit of measurement is pixels, can be mms, cms, inches also
+    const signHere = new docusign.SignHere();
+    signHere.documentId = '1';
+    signHere.pageNumber = '1';
+    signHere.recipientId = '1';
+    signHere.xPosition = '100';
+    signHere.yPosition = '100';
+
+    // can have multiple tabs, so need to add to envelope as a single element list
+    const signHereTabs = [];
+    signHereTabs.push(signHere);
+    const tabs = new docusign.Tabs();
+    tabs.signHereTabs = signHereTabs;
+    signer.tabs = tabs;
+
+    // Above causes issue
+    envDef.recipients = new docusign.Recipients();
+    envDef.recipients.signers = [];
+    envDef.recipients.signers.push(signer);
+
+    // send the envelope (otherwise it will be "created" in the Draft folder
+    envDef.status = 'sent';
+
+    const envelopeSummary = await envelopesApi.createEnvelope(accountId, { envelopeDefinition: envDef });
+    console.log('EnvelopeSummary: ' + JSON.stringify(envelopeSummary, null, 2));
+    return envelopeSummary;
   },
 });
