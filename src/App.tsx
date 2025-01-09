c8aa3e00ff07dc519988d9abd13e06c581a09afb
amitlzkpa <amit.lzkpa@gmail.com>
2025-01-09 15:18:27 +0530

    Move auth and theme stuff to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,9 @@ import '@mantine/core/styles.css';
 import '@mantine/dropzone/styles.css';
 import './index.css';
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-import { ClerkProvider, useAuth } from "@clerk/clerk-react";
-import { ConvexProviderWithClerk } from "convex/react-clerk";
-import { ConvexReactClient } from "convex/react";
+import { useUser } from "@clerk/clerk-react";
 
-import {
-  Flex,
-  MantineProvider,
-  createTheme,
-  rem,
-} from '@mantine/core';
+import { Flex, createTheme, rem } from '@mantine/core';
 
 import Dev from "./views/Dev";
 import OauthCallback_Docusign from "./views/OauthCallback_Docusign";
@@ -22,60 +15,9 @@ import Project from "./views/Project";
 
 import Navbar from "./components/Navbar";
 
-const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
-const CONVEX_URL = import.meta.env.VITE_CONVEX_URL;
-
-const convex = new ConvexReactClient(CONVEX_URL);
-
 function App() {
 
-  const theme = createTheme({
-    fontFamily: 'Nunito, sans-serif',
-    fontFamilyMonospace: 'Monaco, Courier, monospace',
-    headings: {
-      fontWeight: '800',
-      fontFamily: 'Bitter',
-      sizes: {
-        h1: { fontSize: rem(38), lineHeight: '0.9' },
-        h2: { fontSize: rem(34), lineHeight: '0.9' },
-        h3: { fontSize: rem(30), lineHeight: '0.9' },
-        h4: { fontSize: rem(26), lineHeight: '0.9' },
-        h5: { fontSize: rem(22), lineHeight: '0.9' },
-        h6: { fontSize: rem(18), lineHeight: '0.9' },
-      },
-    },
-    fontSizes: {
-      xs: rem(16),
-      sm: rem(17),
-      md: rem(20),
-      lg: rem(22),
-      xl: rem(26),
-    },
-    lineHeights: {
-      xs: '1.6',
-      sm: '1.65',
-      md: '1.75',
-      lg: '1.8',
-      xl: '1.85',
-    },
-    defaultRadius: 'xl',
-    primaryColor: 'versee-purple',
-    colors: {
-      'versee-purple': [
-        "#f3edff",
-        "#e1d7fb",
-        "#beabf0",
-        "#9a7de7",
-        "#7c56de",
-        "#693dd9",
-        "#5f30d8",
-        "#4f23c0",
-        "#461eac",
-        "#3b1898"
-      ],
-    },
-
-  });
+  const user = useUser();
 
   const router = createBrowserRouter([
     {
@@ -83,48 +25,42 @@ function App() {
       element: <Landing />,
     },
     {
-      path: "/home",
-      element: <Home />,
-    },
-    {
-      path: "/p/:projectId?",
-      element: <Project />,
-    },
-    {
-      path: "/my-account",
-      element: <MyAccount />,
-    },
-    {
       path: "/dev",
       element: <Dev />,
     },
     {
+      path: "/home",
+      element: user ? <Home /> : <Landing />,
+    },
+    {
+      path: "/p/:projectId?",
+      element: user ? <Project /> : <Landing />,
+    },
+    {
+      path: "/my-account",
+      element: user ? <MyAccount /> : <Landing />,
+    },
+    {
       path: "/callback/docusign",
-      element: <OauthCallback_Docusign />,
+      element: user ? <OauthCallback_Docusign /> : <Landing />,
     },
   ]);
 
   return (
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
-      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-        <MantineProvider theme={theme}>
-          <Flex
-            w="100%"
-            h="100%"
-            direction="column"
-            align="center"
-            gap="sm"
-          >
-            <Flex w="100%" p="sm">
-              <Navbar />
-            </Flex>
-            <Flex w="100%" h="100%" p="sm" style={{ overflowY: "auto" }}>
-              <RouterProvider router={router} />
-            </Flex>
-          </Flex>
-        </MantineProvider>
-      </ConvexProviderWithClerk>
-    </ClerkProvider>
+    <Flex
+      w="100%"
+      h="100%"
+      direction="column"
+      align="center"
+      gap="sm"
+    >
+      <Flex w="100%" p="sm">
+        <Navbar />
+      </Flex>
+      <Flex w="100%" h="100%" p="sm" style={{ overflowY: "auto" }}>
+        <RouterProvider router={router} />
+      </Flex>
+    </Flex>
   )
 };
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,84 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { ClerkProvider, useAuth, useUser } from "@clerk/clerk-react";
+import { ConvexProviderWithClerk } from "convex/react-clerk";
+import { ConvexReactClient } from "convex/react";
+
+import { MantineProvider, createTheme, rem } from '@mantine/core';
+
+import './index.css';
+import App from './App.tsx';
+
+const Main = () => {
+
+  const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+  const CONVEX_URL = import.meta.env.VITE_CONVEX_URL;
+
+  const convex = new ConvexReactClient(CONVEX_URL);
+
+  const theme = createTheme({
+    fontFamily: 'Nunito, sans-serif',
+    fontFamilyMonospace: 'Monaco, Courier, monospace',
+    headings: {
+      fontWeight: '800',
+      fontFamily: 'Bitter',
+      sizes: {
+        h1: { fontSize: rem(38), lineHeight: '0.9' },
+        h2: { fontSize: rem(34), lineHeight: '0.9' },
+        h3: { fontSize: rem(30), lineHeight: '0.9' },
+        h4: { fontSize: rem(26), lineHeight: '0.9' },
+        h5: { fontSize: rem(22), lineHeight: '0.9' },
+        h6: { fontSize: rem(18), lineHeight: '0.9' },
+      },
+    },
+    fontSizes: {
+      xs: rem(16),
+      sm: rem(17),
+      md: rem(20),
+      lg: rem(22),
+      xl: rem(26),
+    },
+    lineHeights: {
+      xs: '1.6',
+      sm: '1.65',
+      md: '1.75',
+      lg: '1.8',
+      xl: '1.85',
+    },
+    defaultRadius: 'xl',
+    primaryColor: 'versee-purple',
+    colors: {
+      'versee-purple': [
+        "#f3edff",
+        "#e1d7fb",
+        "#beabf0",
+        "#9a7de7",
+        "#7c56de",
+        "#693dd9",
+        "#5f30d8",
+        "#4f23c0",
+        "#461eac",
+        "#3b1898"
+      ],
+    },
+
+  });
+
+  return (
+
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
+        <MantineProvider theme={theme}>
+          <App />
+        </MantineProvider>
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  )
+};
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Main />
   </StrictMode>,
-)
+);
