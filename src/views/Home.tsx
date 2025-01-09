23dfbd798504b4e1040beadce68feb30b298cd01
amitlzkpa <amit.lzkpa@gmail.com>
2025-01-09 19:01:29 +0530

    Button to create new project

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,14 +1,22 @@
-import { Flex } from '@mantine/core';
+import { Button, Flex } from '@mantine/core';
 
 export default function Home() {
 
+  const onClickTest_createNewProject = async () => {
+    console.log("foo");
+  };
+
   return (
     <Flex w="100%" direction="column" align="center" gap="sm">
-      {Array(45)
-        .fill(0)
-        .map((_, index) => (
-          <div key={index}>Home</div>
-        ))}
+      <Flex w="60%" direction="column" align="center" gap="md" p="lg">
+        <Button
+          onClick={onClickTest_createNewProject}
+          w="100%"
+          size="lg"
+        >
+          Create New Project
+        </Button>
+      </Flex>
     </Flex>
   );
 }
