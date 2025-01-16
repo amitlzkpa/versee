63ceb150f2dbeda1a2db94543023ad846613ac05
amitlzkpa <amit.lzkpa@gmail.com>
2025-01-16 19:06:44 +0530

    Morre page reorg

--- src/views/Project.tsx
+++ src/views/Project.tsx
@@ -80,19 +80,11 @@ export default function Project() {
           </Text>
         </Flex>
 
-        <Divider w="100%" />
+        <Divider w="100%" my="lg" />
 
         <Flex direction="column">
           <Text size="md" fw="bold">
-            Upload Agreement
-          </Text>
-        </Flex>
-
-        <Divider w="100%" />
-
-        <Flex direction="column">
-          <Text size="md" fw="bold">
-            Upload Project Files
+            Agreement Data
           </Text>
         </Flex>
 
@@ -208,13 +200,125 @@ export default function Project() {
           Upload Files
         </Button>
 
-        <Divider w="100%" />
+        <Divider w="100%" my="lg" />
 
         <Flex direction="column">
           <Text size="md" fw="bold">
-            Existing Project Files
+            Project Files
           </Text>
         </Flex>
+
+        <Dropzone
+          openRef={openRef}
+          activateOnClick={false}
+          loading={isUploading}
+          onDrop={addToDroppedFiles}
+          accept={[
+            'image/png',
+            'image/jpeg',
+            'image/webp',
+            'image/heic',
+            'text/csv',
+            'application/pdf',
+            'application/msword',
+            'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+          ]}
+          maxSize={15 * 1024 * 1024}
+          disabled={!projectId}
+        >
+          <div
+            style={{
+              position: "absolute",
+              width: "100%",
+              height: "100%",
+              pointerEvents: "all",
+              cursor: "pointer"
+            }}
+            onClick={() => { openRef.current?.() }}
+          />
+          <Flex
+            h="180"
+            direction="column"
+            justify="center"
+            align="center"
+            gap="md"
+            p="sm"
+            style={{ pointerEvents: 'none', position: "relative" }}
+          >
+            <Dropzone.Accept>
+              <FaFileDownload
+                style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-blue-6)' }}
+              />
+            </Dropzone.Accept>
+            <Dropzone.Reject>
+              <FaMinusCircle
+                style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-red-6)' }}
+              />
+            </Dropzone.Reject>
+            <Dropzone.Idle>
+              <Flex w="100%" h="100%">
+                {
+                  droppedFiles.length > 0
+                    ?
+                    (
+                      <Flex w="100%" direction="column" align="center" gap="sm">
+                        <Text fz="md" fw="bold">Selected Files</Text>
+                        <Flex w="100%" direction="column" gap="xs" style={{ overflowY: "auto", pointerEvents: "auto" }}>
+                          {droppedFiles.map((f, i) =>
+                          (
+                            <Flex key={`${f.name}_${i}`} gap="xs" align="center">
+                              <FaTrash
+                                style={{
+                                  pointerEvents: 'all',
+                                  width: rem(12),
+                                  height: rem(12),
+                                  color: 'var(--mantine-color-gray-5)',
+                                  cursor: "pointer"
+                                }}
+                                onClick={() => { removeFromDroppedFiles(f) }}
+                              />
+                              <Text fz="sm" c="dimmed">{i + 1}.</Text>
+                              <Text>{f.name}</Text>
+                            </Flex>
+                          )
+                          )}
+                        </Flex>
+                      </Flex>
+                    )
+                    :
+                    (
+                      <Flex
+                        direction="column"
+                        justify="center"
+                        align="center"
+                        gap="sm"
+                        style={{ textAlign: "center" }}
+                      >
+                        <FaFileImport
+                          style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-dimmed)' }}
+                        />
+                        <Text size="xl" inline>
+                          Drag images here or click to select files
+                        </Text>
+                        <Text size="sm" c="dimmed" inline>
+                          Attach as many files as you like
+                        </Text>
+                      </Flex>
+                    )
+                }
+              </Flex>
+            </Dropzone.Idle>
+          </Flex>
+        </Dropzone>
+
+        <Button
+          onClick={onClick_uploadDroppedFilesToSrcDocs}
+          w="100%"
+          size="lg"
+          disabled={!projectId && droppedFiles.length < 1}
+        >
+          Upload Files
+        </Button>
 
         <Flex w="100%" direction="column" align="center" gap="xs">
           {
@@ -234,6 +338,7 @@ export default function Project() {
 
                       <Button
                         component="a"
+                        variant="outline"
                         href={srcDoc.fileUrl}
                         target="_blank"
                         w="100%"
@@ -242,6 +347,7 @@ export default function Project() {
                         Open
                       </Button>
                       <Button
+                        variant="outline"
                         onClick={() => {
                           performAction_analyseSrcDoc({
                             srcDocId: srcDoc._id
@@ -253,6 +359,7 @@ export default function Project() {
                         Analyse
                       </Button>
                       <Button
+                        variant="outline"
                         onClick={() => {
                           performAction_sendDocusignSigningEmail({
                             srcDocId: srcDoc._id
