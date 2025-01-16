248a8bb59a85bae86b7ef8f9177956a613aa2120
amitlzkpa <amit.lzkpa@gmail.com>
2025-01-16 19:14:37 +0530

    Move FileUploader to new component

---
+++ src/components/FileUploader.tsx
@@ -0,0 +1,173 @@
+import { useRef, useState } from 'react';
+import { Button, Flex, Text, rem } from '@mantine/core';
+import { Dropzone } from '@mantine/dropzone';
+
+import { FaFileDownload, FaFileImport, FaMinusCircle, FaTrash } from 'react-icons/fa';
+
+import { useAction } from "convex/react";
+import { api } from "../../convex/_generated/api";
+
+export default function FileUploader({ projectId = null }: any) {
+
+  const openRef = useRef<() => void>(null);
+
+  const [droppedFiles, setDroppedFiles] = useState<any[]>([]);
+
+  const [isUploading, setIsUploading] = useState(false);
+
+  const performAction_createNewSrcDoc = useAction(api.vsActions.createNewSrcDoc);
+
+  const performAction_generateUploadUrl = useAction(api.vsActions.generateUploadUrl);
+
+
+
+  const addToDroppedFiles = (files: any) => {
+    setDroppedFiles(prev => [...prev, ...files]);
+  };
+
+  const removeFromDroppedFiles = (file: any) => {
+    setDroppedFiles(droppedFiles.filter((f) => f !== file));
+  };
+
+  const onClick_uploadDroppedFilesToSrcDocs = async () => {
+    setIsUploading(true);
+
+    const ps = droppedFiles.map((file: any) => new Promise((resolve, reject) => {
+      performAction_generateUploadUrl()
+        .then(async (uploadUrl) => {
+          try {
+            const result = await fetch(uploadUrl, {
+              method: "POST",
+              body: file,
+            });
+            const uploadedCvxFile = await result.json();
+            const cvxStoredFileId = uploadedCvxFile.storageId;
+            const newSrcDocId = await performAction_createNewSrcDoc({ projectId: projectId, cvxStoredFileId })
+            return resolve(newSrcDocId);
+          } catch (err) {
+            return reject(err);
+          }
+        });
+    }));
+
+    const srcDocIds = (await Promise.allSettled(ps)).filter(r => r.status === "fulfilled").map(r => r.value);
+    setDroppedFiles([]);
+    setIsUploading(false);
+  };
+
+  return (
+    <>
+      <Dropzone
+        openRef={openRef}
+        activateOnClick={false}
+        loading={isUploading}
+        onDrop={addToDroppedFiles}
+        accept={[
+          'image/png',
+          'image/jpeg',
+          'image/webp',
+          'image/heic',
+          'text/csv',
+          'application/pdf',
+          'application/msword',
+          'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+        ]}
+        maxSize={15 * 1024 * 1024}
+        disabled={!projectId}
+      >
+        <div
+          style={{
+            position: "absolute",
+            width: "100%",
+            height: "100%",
+            pointerEvents: "all",
+            cursor: "pointer"
+          }}
+          onClick={() => { openRef.current?.() }}
+        />
+        <Flex
+          h="180"
+          direction="column"
+          justify="center"
+          align="center"
+          gap="md"
+          p="sm"
+          style={{ pointerEvents: 'none', position: "relative" }}
+        >
+          <Dropzone.Accept>
+            <FaFileDownload
+              style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-blue-6)' }}
+            />
+          </Dropzone.Accept>
+          <Dropzone.Reject>
+            <FaMinusCircle
+              style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-red-6)' }}
+            />
+          </Dropzone.Reject>
+          <Dropzone.Idle>
+            <Flex w="100%" h="100%">
+              {
+                droppedFiles.length > 0
+                  ?
+                  (
+                    <Flex w="100%" direction="column" align="center" gap="sm">
+                      <Text fz="md" fw="bold">Selected Files</Text>
+                      <Flex w="100%" direction="column" gap="xs" style={{ overflowY: "auto", pointerEvents: "auto" }}>
+                        {droppedFiles.map((f, i) =>
+                        (
+                          <Flex key={`${f.name}_${i}`} gap="xs" align="center">
+                            <FaTrash
+                              style={{
+                                pointerEvents: 'all',
+                                width: rem(12),
+                                height: rem(12),
+                                color: 'var(--mantine-color-gray-5)',
+                                cursor: "pointer"
+                              }}
+                              onClick={() => { removeFromDroppedFiles(f) }}
+                            />
+                            <Text fz="sm" c="dimmed">{i + 1}.</Text>
+                            <Text>{f.name}</Text>
+                          </Flex>
+                        )
+                        )}
+                      </Flex>
+                    </Flex>
+                  )
+                  :
+                  (
+                    <Flex
+                      direction="column"
+                      justify="center"
+                      align="center"
+                      gap="sm"
+                      style={{ textAlign: "center" }}
+                    >
+                      <FaFileImport
+                        style={{ width: rem(52), height: rem(52), color: 'var(--mantine-color-dimmed)' }}
+                      />
+                      <Text size="xl" inline>
+                        Drag images here or click to select files
+                      </Text>
+                      <Text size="sm" c="dimmed" inline>
+                        Attach as many files as you like
+                      </Text>
+                    </Flex>
+                  )
+              }
+            </Flex>
+          </Dropzone.Idle>
+        </Flex>
+      </Dropzone>
+
+      <Button
+        onClick={onClick_uploadDroppedFilesToSrcDocs}
+        w="100%"
+        size="lg"
+        disabled={!projectId && droppedFiles.length < 1}
+      >
+        Upload Files
+      </Button>
+    </>
+  );
+};
