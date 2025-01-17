6b47bf2da02b72b93b27c6a26f4483e3a4bc46c9
amitlzkpa <amit.lzkpa@gmail.com>
2025-01-17 12:48:20 +0530

    Small fixes

--- src/views/Project.tsx
+++ src/views/Project.tsx
@@ -14,6 +14,7 @@ import {
 import { useAction, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
 import { Id } from '../../convex/_generated/dataModel';
+
 import ProjectInit_Uninit from '../components/ProjectInit_Uninit';
 import FileUploader from '../components/FileUploader';
 
@@ -98,7 +99,7 @@ export default function Project() {
         {
           (curProjectSrcDocs ?? []).length < 1
             ?
-            <ProjectInit_Uninit />
+            <ProjectInit_Uninit projectId={currProject._id} />
             :
             <>
               <Divider w="100%" />
