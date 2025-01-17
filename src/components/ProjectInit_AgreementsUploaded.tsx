d5a89ec1f6e65d7b5e8790deeb8a15b1deafe11a
amitlzkpa <amit.lzkpa@gmail.com>
2025-01-17 15:40:45 +0530

    Update styling of embedded doc viewer in carousel

--- src/components/ProjectInit_AgreementsUploaded.tsx
+++ src/components/ProjectInit_AgreementsUploaded.tsx
@@ -34,76 +34,62 @@ export default function ProjectInit_AgreementsUploaded({
         direction="column"
         align="center"
         gap="sm"
+        py="lg"
       >
-        <Flex
+        <Text fz="xl" fw="bold" w="100%">
+          Agreement Papers
+          ({(curProjectSrcDocs ?? []).length})
+        </Text>
+        <Carousel
           w="100%"
           h="100%"
-          mih="400"
-          direction="column"
-          align="center"
-          gap="sm"
-          style={{ border: "1px solid red" }}
+          slideSize="80%"
+          slideGap="md"
+          withIndicators={true}
         >
-          <Text fz="xl" fw="bold" w="100%">Agreement Papers</Text>
-          <Carousel
-            w="100%"
-            h="100%"
-            slideSize="80%"
-            slideGap="md"
-            style={{ border: "1px solid green" }}
-          >
-            {
-              (curProjectSrcDocs ?? [])
-                .map((srcDoc: any) => {
-                  return (
-                    <Carousel.Slide key={srcDoc._id}>
-                      <Flex
+          {
+            (curProjectSrcDocs ?? [])
+              .map((srcDoc: any) => {
+                return (
+                  <Carousel.Slide key={srcDoc._id}>
+                    <Flex
+                      w="100%"
+                      h="100%"
+                      direction="column"
+                      align="center"
+                      gap="sm"
+                      style={{ overflowY: "auto" }}
+                    >
+                      <Text fz="sm">{srcDoc._id}</Text>
+                      <Text fw="bold">{srcDoc.titleText}</Text>
+
+                      <embed
+                        style={{
+                          width: "100%",
+                          height: rem(400),
+                          borderRadius: rem(20)
+                        }}
+                        src={srcDoc.fileUrl}
+                      />
+
+                      <Button
+                        component="a"
+                        variant="transparent"
+                        href={srcDoc.fileUrl}
+                        target="_blank"
                         w="100%"
-                        h="100%"
-                        direction="column"
-                        align="center"
-                        gap="sm"
-                        style={{ border: "1px solid blue", overflowY: "auto" }}
+                        size="sm"
                       >
-                        <Text fz="sm">{srcDoc._id}</Text>
-                        <Text fw="bold">{srcDoc.titleText}</Text>
+                        Open
+                      </Button>
 
-                        <embed
-                          style={{ width: "100%", height: "100%", minHeight: "400px" }}
-                          src={srcDoc.fileUrl}
-                        />
-
-                        <Text>{srcDoc.summaryText}</Text>
-
-                        <Button
-                          component="a"
-                          variant="outline"
-                          href={srcDoc.fileUrl}
-                          target="_blank"
-                          w="100%"
-                          size="lg"
-                        >
-                          Open
-                        </Button>
-                        <Button
-                          variant="outline"
-                          onClick={() => {
-                            cvxUtils.performAction_sendDocusignSigningEmail({
-                              srcDocId: srcDoc._id
-                            });
-                          }}
-                          w="100%"
-                          size="lg"
-                        >
-                          Send
-                        </Button>
-                      </Flex>
-                    </Carousel.Slide>
-                  );
-                })
-            }
-          </Carousel>
-        </Flex>
+                      <Text>{srcDoc.summaryText}</Text>
+                    </Flex>
+                  </Carousel.Slide>
+                );
+              })
+          }
+        </Carousel>
       </Flex>
     </>
   );
