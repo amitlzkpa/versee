10ae9b63297386eef13984acea287390dff111f8
amitlzkpa <amit.lzkpa@gmail.com>
2025-01-21 10:38:17 +0530

    Show agreement and submitted files page

--- src/views/Project.tsx
+++ src/views/Project.tsx
@@ -22,7 +22,13 @@ import ProjectInit_SignersAssigned from "../components/ProjectInit_SignersAssign
 import ProjectInit_TaggingCompleted from "../components/ProjectInit_TaggingCompleted";
 import ProjectInit_AgreementSent from "../components/ProjectInit_AgreementSent";
 
+import FileUploader from "../components/FileUploader";
+
+import useCvxUtils from "../hooks/cvxUtils";
+
 export default function Project() {
+  const cvxUtils = useCvxUtils();
+
   // PROJECT
 
   const { projectId } = useParams();
@@ -31,6 +37,53 @@ export default function Project() {
     api.dbOps.getProject_ByProjectId,
     projectId ? { projectId: projectId as Id<"vsProjects"> } : "skip"
   );
+
+  // SRCDOC
+
+  const curProjectSrcDocs = useQuery(
+    api.dbOps.getAllSrcDocs_ForProject,
+    projectId ? { projectId: projectId as Id<"vsProjects"> } : "skip"
+  );
+
+  // PRJFILE
+
+  const curProjectPrjFiles = useQuery(
+    api.dbOps.getAllPrjFiles_ForProject,
+    projectId ? { projectId: projectId as Id<"vsProjects"> } : "skip"
+  );
+
+  const performAction_createNewPrjFile = useAction(
+    api.vsActions.createNewPrjFile
+  );
+
+  const onClick_uploadFiles_PrjFiles = async (droppedFiles: any) => {
+    const ps = droppedFiles.map(
+      (file: any) =>
+        new Promise((resolve, reject) => {
+          cvxUtils.performAction_generateUploadUrl().then(async (uploadUrl) => {
+            try {
+              const result = await fetch(uploadUrl, {
+                method: "POST",
+                body: file,
+              });
+              const uploadedCvxFile = await result.json();
+              const cvxStoredFileId = uploadedCvxFile.storageId;
+              const newPrjFileId = await performAction_createNewPrjFile({
+                projectId: currProject?._id,
+                cvxStoredFileId,
+              });
+              return resolve(newPrjFileId);
+            } catch (err) {
+              return reject(err);
+            }
+          });
+        })
+    );
+
+    const newPrjFileIds = (await Promise.allSettled(ps))
+      .filter((r) => r.status === "fulfilled")
+      .map((r) => r.value);
+  };
 
   return (
     <Flex w="100%" direction="column" align="center" gap="sm" p="lg">
@@ -72,6 +125,96 @@ export default function Project() {
               return null;
           }
         })()}
+
+        {projectId ? (
+          <Accordion defaultValue="upload-srcdoc">
+            <Accordion.Item key="list-srcdocs" value="list-srcdocs">
+              <Accordion.Control>
+                <Text size="md" fw="bold">
+                  Doocuments
+                </Text>
+              </Accordion.Control>
+              <Accordion.Panel>
+                <Flex w="100%" direction="column" align="center" gap="xs">
+                  {(curProjectSrcDocs ?? []).map((srcDoc: any) => {
+                    return (
+                      <Card key={srcDoc._id} w="100%" withBorder radius="xl">
+                        <Flex direction="column" align="stretch" gap="sm">
+                          <Text fz="sm">{srcDoc._id}</Text>
+                          <Text fw="bold">{srcDoc.titleText}</Text>
+                          <Text>{srcDoc.summaryText}</Text>
+
+                          <Button
+                            component="a"
+                            variant="outline"
+                            href={srcDoc.fileUrl}
+                            target="_blank"
+                            w="100%"
+                            size="lg"
+                          >
+                            Open
+                          </Button>
+                        </Flex>
+                      </Card>
+                    );
+                  })}
+                </Flex>
+              </Accordion.Panel>
+            </Accordion.Item>
+
+            <Accordion.Item key="list-prjfiles" value="list-prjfiles">
+              <Accordion.Control>
+                <Text size="md" fw="bold">
+                  Received Files
+                </Text>
+              </Accordion.Control>
+              <Accordion.Panel>
+                <Flex w="100%" direction="column" align="center" gap="xs">
+                  {(curProjectPrjFiles ?? []).map((prjFile: any) => {
+                    return (
+                      <Card key={prjFile._id} w="100%" withBorder radius="xl">
+                        <Flex direction="column" align="stretch" gap="sm">
+                          <Text fz="sm">{prjFile._id}</Text>
+                          <Text fw="bold">{prjFile.titleText}</Text>
+                          <Text>{prjFile.summaryText}</Text>
+
+                          <Button
+                            component="a"
+                            variant="outline"
+                            href={prjFile.fileUrl}
+                            target="_blank"
+                            w="100%"
+                            size="lg"
+                          >
+                            Open
+                          </Button>
+                        </Flex>
+                      </Card>
+                    );
+                  })}
+                </Flex>
+              </Accordion.Panel>
+            </Accordion.Item>
+
+            <Accordion.Item key="upload-prjfile" value="upload-prjfile">
+              <Accordion.Control>
+                <Text size="md" fw="bold">
+                  Upload Project File
+                </Text>
+              </Accordion.Control>
+              <Accordion.Panel>
+                <Flex direction="column">
+                  <FileUploader
+                    projectId={currProject?._id}
+                    onClick_uploadFiles={onClick_uploadFiles_PrjFiles}
+                  />
+                </Flex>
+              </Accordion.Panel>
+            </Accordion.Item>
+          </Accordion>
+        ) : (
+          <></>
+        )}
 
         <Divider w="100%" />
       </Flex>
