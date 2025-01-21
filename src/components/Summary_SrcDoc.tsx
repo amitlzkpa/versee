cbd4e583d2f82208da220d21d8cf0007f0725a5c
amitlzkpa <amit.lzkpa@gmail.com>
2025-01-21 10:52:10 +0530

    New component for multiple languages

---
+++ src/components/Summary_SrcDoc.tsx
@@ -0,0 +1,42 @@
+import {
+  Accordion,
+  Button,
+  Card,
+  Center,
+  Divider,
+  Flex,
+  Text,
+  rem,
+} from "@mantine/core";
+
+import { useAction, useQuery } from "convex/react";
+import { api } from "../../convex/_generated/api";
+import { Id } from "../../convex/_generated/dataModel";
+
+import useCvxUtils from "../hooks/cvxUtils";
+
+export default function ProjectInit_Uninit({ srcDocId = null }: any) {
+  const cvxUtils = useCvxUtils();
+
+  const srcDoc = useQuery(api.dbOps.getSrcDoc_BySrcDocId, { srcDocId });
+
+  return (
+    <>
+      <Flex direction="column" w="100%" gap="md">
+        <Flex direction="column" w="100%">
+          <Text fw="bold" fz="sm">
+            English
+          </Text>
+          <Text>{srcDoc.summaryText}</Text>
+        </Flex>
+
+        <Flex direction="column" w="100%">
+          <Text fw="bold" fz="sm">
+            Spanish
+          </Text>
+          <Text>{srcDoc.summary_es_Text}</Text>
+        </Flex>
+      </Flex>
+    </>
+  );
+}
