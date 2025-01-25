aa9c85b8e4788f0f0c84a406d30fbe17b84ad962
amitlzkpa <amit.lzkpa@gmail.com>
2025-01-26 02:32:30 +0530

    Cleanup and sort out dev file

--- src/views/Dev.tsx
+++ src/views/Dev.tsx
@@ -104,6 +104,7 @@ export default function Dev() {
               const cvxStoredFileId = uploadedCvxFile.storageId;
               const newPrjFileId = await performAction_createNewPrjFile({
                 projectId: currProject?._id,
+                applicationId: "" as Id<"vsApplications">,
                 cvxStoredFileId,
               });
               return resolve(newPrjFileId);
@@ -152,134 +153,6 @@ export default function Dev() {
           >
             G Sheets Test
           </Button>
-
-          {/*
-          <Accordion defaultValue="upload-srcdoc">
-            <Accordion.Item key="upload-srcdoc" value="upload-srcdoc">
-              <Accordion.Control>
-                <Text size="md" fw="bold">
-                  Upload Associated Agreement
-                </Text>
-              </Accordion.Control>
-              <Accordion.Panel>
-                <Flex direction="column">
-                  <FileUploader
-                    projectId={currProject?._id}
-                    onClick_uploadFiles={onClick_uploadFiles_SrcDoc}
-                  />
-                </Flex>
-              </Accordion.Panel>
-            </Accordion.Item>
-
-            <Accordion.Item key="list-srcdocs" value="list-srcdocs">
-              <Accordion.Control>
-                <Text size="md" fw="bold">
-                  View Agreements
-                </Text>
-              </Accordion.Control>
-              <Accordion.Panel>
-                <Flex w="100%" direction="column" align="center" gap="xs">
-                  {(curProjectSrcDocs ?? []).map((srcDoc: any) => {
-                    return (
-                      <Card key={srcDoc._id} w="100%" withBorder radius="xl">
-                        <Flex direction="column" align="stretch" gap="sm">
-                          <Text fz="sm">{srcDoc._id}</Text>
-                          <Text fw="bold">{srcDoc.titleText}</Text>
-                          <Text>{srcDoc.summaryText}</Text>
-
-                          <Button
-                            component="a"
-                            variant="outline"
-                            href={srcDoc.fileUrl}
-                            target="_blank"
-                            w="100%"
-                            size="lg"
-                          >
-                            Open
-                          </Button>
-                          <Button
-                            variant="outline"
-                            onClick={() => {
-                              performAction_analyseSrcDoc({
-                                srcDocId: srcDoc._id,
-                              });
-                            }}
-                            w="100%"
-                            size="lg"
-                          >
-                            Analyse
-                          </Button>
-                          <Button
-                            variant="outline"
-                            onClick={() => {
-                              cvxUtils.performAction_sendDocusignSigningEmail({
-                                srcDocId: srcDoc._id,
-                              });
-                            }}
-                            w="100%"
-                            size="lg"
-                          >
-                            Send
-                          </Button>
-                        </Flex>
-                      </Card>
-                    );
-                  })}
-                </Flex>
-              </Accordion.Panel>
-            </Accordion.Item>
-
-            <Accordion.Item key="upload-prjfile" value="upload-prjfile">
-              <Accordion.Control>
-                <Text size="md" fw="bold">
-                  Upload Project Files
-                </Text>
-              </Accordion.Control>
-              <Accordion.Panel>
-                <Flex direction="column">
-                  <FileUploader
-                    projectId={currProject?._id}
-                    onClick_uploadFiles={onClick_uploadFiles_PrjFiles}
-                  />
-                </Flex>
-              </Accordion.Panel>
-            </Accordion.Item>
-
-            <Accordion.Item key="list-prjfiles" value="list-prjfiles">
-              <Accordion.Control>
-                <Text size="md" fw="bold">
-                  View Project Files
-                </Text>
-              </Accordion.Control>
-              <Accordion.Panel>
-                <Flex w="100%" direction="column" align="center" gap="xs">
-                  {(curProjectPrjFiles ?? []).map((prjFile: any) => {
-                    return (
-                      <Card key={prjFile._id} w="100%" withBorder radius="xl">
-                        <Flex direction="column" align="stretch" gap="sm">
-                          <Text fz="sm">{prjFile._id}</Text>
-                          <Text fw="bold">{prjFile.titleText}</Text>
-                          <Text>{prjFile.summaryText}</Text>
-
-                          <Button
-                            component="a"
-                            variant="outline"
-                            href={prjFile.fileUrl}
-                            target="_blank"
-                            w="100%"
-                            size="lg"
-                          >
-                            Open
-                          </Button>
-                        </Flex>
-                      </Card>
-                    );
-                  })}
-                </Flex>
-              </Accordion.Panel>
-            </Accordion.Item>
-          </Accordion>
-          */}
         </Flex>
       </Authenticated>
     </Flex>
