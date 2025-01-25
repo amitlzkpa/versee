a1e1ca39767c12a096964e9ffcd231bae957600e
amitlzkpa <amit.lzkpa@gmail.com>
2025-01-25 20:22:39 +0530

    3 Pane submit page

--- src/views/Submit.tsx
+++ src/views/Submit.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import { Button, Divider, Flex, Text, rem } from "@mantine/core";
+import { Accordion, Button, Divider, Flex, Text, rem } from "@mantine/core";
 
 import { useAction, useQuery } from "convex/react";
 import { api } from "../../convex/_generated/api";
@@ -29,16 +29,44 @@ export default function Submit() {
     projectId ? { projectId: projectId as Id<"vsProjects"> } : "skip"
   );
 
-  const [currUserHasAccess, setCurrUserHasAccess] = useState(true);
+  const [srcDoc, setSrcDoc] = useState<any>({});
 
   useEffect(() => {
-    const currUserEmail = storedUserData?.user?.email;
-    if (!currUserEmail) return;
-    const signerEmails = (currProject?.signers ?? []).map(
-      (s: any) => s.signerEmail
-    );
-    setCurrUserHasAccess(signerEmails.includes(currUserEmail));
-  }, [currProject, storedUserData?.user?.email]);
+    if ((curProjectSrcDocs ?? []).length < 1) return;
+    const docOne: any = (curProjectSrcDocs ?? [])[0];
+    setSrcDoc(docOne);
+  }, [curProjectSrcDocs]);
+
+  const [offeringsJSON, setOfferingsJSON] = useState([]);
+
+  useEffect(() => {
+    if ((curProjectSrcDocs ?? []).length < 1) return;
+    const docOne: any = (curProjectSrcDocs ?? [])[0];
+    if (!docOne?.offerings_Text) return;
+    setOfferingsJSON(JSON.parse(docOne.offerings_Text));
+  }, [curProjectSrcDocs]);
+
+  const [criteriaJSON, setCriteriaJSON] = useState([]);
+
+  useEffect(() => {
+    if ((curProjectSrcDocs ?? []).length < 1) return;
+    const docOne: any = (curProjectSrcDocs ?? [])[0];
+    if (!docOne?.criteria_Text) return;
+    setCriteriaJSON(JSON.parse(docOne.criteria_Text));
+  }, [curProjectSrcDocs]);
+
+  // ACCESS
+
+  const [currUserHasAccess, setCurrUserHasAccess] = useState(true);
+
+  // useEffect(() => {
+  //   const currUserEmail = storedUserData?.user?.email;
+  //   if (!currUserEmail) return;
+  //   const signerEmails = (currProject?.signers ?? []).map(
+  //     (s: any) => s.signerEmail
+  //   );
+  //   setCurrUserHasAccess(signerEmails.includes(currUserEmail));
+  // }, [currProject, storedUserData?.user?.email]);
 
   // PRJFILE
 
@@ -78,15 +106,119 @@ export default function Submit() {
   };
 
   return (
-    <Flex w="100%" direction="column" align="center" gap="sm">
-      <Flex w="60%" direction="column" align="center" gap="md" p="lg">
-        <Text>Submit</Text>
-        <Text fz="lg">Use this page to submit files.</Text>
+    <Flex w="100%" gap="sm">
+      <Flex w="40%" direction="column" align="stretch" gap="sm">
+        <Text fz="xl" fw="bold">
+          Submit Application
+        </Text>
+
+        <Divider />
+
+        <Text>{srcDoc.summaryText}</Text>
+
+        <Flex
+          w="100%"
+          h="100%"
+          direction="column"
+          align="stretch"
+          style={{ overflowY: "auto" }}
+        >
+          <Accordion w="100%">
+            <Accordion.Item key="first" value="first">
+              <Accordion.Control>
+                <Text>Scheme</Text>
+              </Accordion.Control>
+              <Accordion.Panel>
+                {offeringsJSON ? (
+                  <Flex w="100%" direction="column" align="stretch" gap="md">
+                    {offeringsJSON.map((offerItem: any, offerIdx: number) => {
+                      return (
+                        <Flex
+                          w="100%"
+                          direction="column"
+                          align="center"
+                          gap="xs"
+                          key={offerItem.title}
+                        >
+                          <Flex w="100%" align="center" gap="sm">
+                            <Text>{offerItem.title}</Text>
+                          </Flex>
+                          <Flex gap="sm">
+                            <Text>{offerItem.quantity}</Text>
+                            <Text>{offerItem.units}</Text>
+                          </Flex>
+                          <Text>{offerItem.description}</Text>
+                        </Flex>
+                      );
+                    })}
+                  </Flex>
+                ) : (
+                  <></>
+                )}
+              </Accordion.Panel>
+            </Accordion.Item>
+
+            <Accordion.Item key="second" value="second">
+              <Accordion.Control>
+                <Text>Eligibility</Text>
+              </Accordion.Control>
+              <Accordion.Panel>
+                {criteriaJSON ? (
+                  <Flex w="100%" direction="column" align="stretch" gap="md">
+                    {criteriaJSON.map(
+                      (criteriaItem: any, criteriaIdx: number) => {
+                        return (
+                          <Flex
+                            w="100%"
+                            direction="column"
+                            align="stretch"
+                            gap="xs"
+                            key={criteriaItem.title}
+                          >
+                            <Flex w="100%" align="center" gap="sm">
+                              <Text>{criteriaItem.title}</Text>
+                            </Flex>
+                            <Flex w="100%" gap="sm" px="sm">
+                              <Text fz="md">Applies To:</Text>
+                              <Text>{criteriaItem.applies_to}</Text>
+                            </Flex>
+                            <Text>{criteriaItem.description}</Text>
+                          </Flex>
+                        );
+                      }
+                    )}
+                  </Flex>
+                ) : (
+                  <></>
+                )}
+              </Accordion.Panel>
+            </Accordion.Item>
+          </Accordion>
+        </Flex>
       </Flex>
 
-      <Divider w="60%" my="lg" />
+      <Flex
+        w="40%"
+        h="100%"
+        direction="column"
+        align="stretch"
+        gap="md"
+        style={{ overflowY: "auto" }}
+      >
+        <Text fw="bold" style={{ textAlign: "center" }}>
+          {srcDoc.titleText}
+        </Text>
+        <embed
+          style={{
+            width: "100%",
+            height: "100%",
+            borderRadius: rem(20),
+          }}
+          src={srcDoc.fileUrl}
+        />
+      </Flex>
 
-      <Flex w="60%" direction="column" align="center" gap="md" p="lg">
+      <Flex w="20%" direction="column" align="center" gap="md">
         {currUserHasAccess ? (
           <>
             <FileUploader
