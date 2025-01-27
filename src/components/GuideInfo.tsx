312ca2fa6b9110957f8b0b863a4efc9b2d553dcf
amitlzkpa <amit.lzkpa@gmail.com>
2025-01-27 18:34:34 +0530

    Add purple color to guide-info

--- src/components/GuideInfo.tsx
+++ src/components/GuideInfo.tsx
@@ -1,26 +1,24 @@
-
-import { Flex, Text } from "@mantine/core";
+import { Flex, Paper, Text } from "@mantine/core";
 
 export default function GuideInfo({ msgHead, msgDesc }: any) {
-
   return (
-    <Flex
-      w="100%"
-      h="100%"
-      maw="400"
-      direction="column"
-      justify="center"
-      align="center"
-      gap="sm"
-      p="lg"
-      style={{ textAlign: "center" }}
-    >
-      <Text lh="1">
-        {msgHead}
-      </Text>
-      <Text fz="lg" lh="1.2">
-        {msgDesc}
-      </Text>
-    </Flex>
+    <Paper w="100%" h="100%" bg="versee-purple" c="white">
+      <Flex
+        w="100%"
+        h="100%"
+        maw="400"
+        direction="column"
+        justify="center"
+        align="center"
+        gap="sm"
+        p="lg"
+        style={{ textAlign: "center" }}
+      >
+        <Text lh="1">{msgHead}</Text>
+        <Text fz="lg" fw="bold" lh="1.2">
+          {msgDesc}
+        </Text>
+      </Flex>
+    </Paper>
   );
 }
